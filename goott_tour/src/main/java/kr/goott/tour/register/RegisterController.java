65d18dbfa68ff8984b501a36131f550980c3ad86
shaqed89 <hyungjae77@gmail.com>
2019-11-18 21:56:34 +0900

    커밋

---
+++ goott_tour/src/main/java/kr/goott/tour/register/RegisterController.java
@@ -0,0 +1,67 @@
+package kr.goott.tour.register;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class RegisterController {
+	@Autowired
+	SqlSession sqlSession;
+	
+	//아이디 중복검사
+	@RequestMapping("/register/idCheck")
+	@ResponseBody
+	public String idCheck(@RequestParam("userId") String userId) {		
+//		System.out.println(userId);
+		RegisterDAOInterface dao = sqlSession.getMapper(RegisterDAOInterface.class);
+		
+		int result = dao.idCheck(userId); //아이디 중복검사
+//		System.out.println(result);
+		
+		if(result>0) return null; //아이디가 중복일 경우
+		return userId; //아이디가 중복이 아닐경우
+	}
+
+	//회원가입 폼
+	@RequestMapping("/register/registerForm")
+	public String regForm() {
+		return "register/registerForm";
+	}
+	
+	//회원가입
+	@RequestMapping(value="/register/registerOk", method=RequestMethod.POST)
+	public ModelAndView insertRecord(RegisterVO vo, HttpServletRequest request) {
+		
+		RegisterDAOInterface dao = sqlSession.getMapper(RegisterDAOInterface.class);
+		System.out.println(vo.getUserName());
+		ModelAndView mav = new ModelAndView();
+		try{
+			int cnt = dao.insertRecord(vo);
+			
+			if(cnt>0) {
+				mav.setViewName("register/registerOk"); 
+			}else {
+				mav.setViewName("redirect:registerForm");
+			}
+		}catch(Exception e) {
+			System.out.println("회원가입에러.."+e.getMessage());
+			mav.setViewName("register/registerError");
+		}
+			return mav;
+	}
+	
+	//회원가입 에러
+	@RequestMapping("/register/registerError")
+	public String regErr() {
+		return "register/registerError";
+	}
+	
+}
